c3b771ecc4460be21103bdfdc559ccc6e44bfdf1
abhishekmore <abhishekmore1506@gmail.com>
2023-09-10 10:59:19 +0530

    Arithmatic operation in Java

---
+++ FirstJava/src/com/ArithmaticOperation.java
@@ -0,0 +1,26 @@
+package com;
+
+/**
+ * @author Abhishek More
+ *
+ */
+
+public class ArithmaticOperation  {
+
+public static void main(String[] args) {
+int a=10,b=5, sum, sub, mul, div, mod;
+   sum=a+b;
+sub=a-b;
+   mul=a*b;
+div=a/b;
+mod=a%b;
+System.out.println(" Arithmatic Operation");
+System.out.println("Additation=" +sum);
+System.out.println("Substraction=" +sub);
+System.out.println("Multiplicatation=" +mul);
+System.out.println("Division=" +div);
+System.out.println("Modulus=" +mod);
+
+}
+
+}
---
+++ FirstJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ambition
+ *
+ */
+module FirstJava {
+}
